ccc51a2b99b0baffa2f474228eec13ad97817ae2
Pedro Sereno <psg2@cin.ufpe.br>
2017-07-10 01:51:12 -0300

    New Heatmap working

--- src/app/heatmap.ts
+++ src/app/heatmap.ts
@@ -9,6 +9,7 @@ const colors = ["#ffffd9", "#7fcdbb", "#41b6c4", "#1d91c0", "#225ea8", "#253494"
 export class HeatMap extends AbstractPlot {
     private plot;
     private grid;
+    private latLng;
 
     constructor(parent: d3.Selection<BaseType, {}, HTMLElement, any>,
                 x: number,
@@ -25,32 +26,12 @@ export class HeatMap extends AbstractPlot {
         super(parent, x, y, totalWidth, totalHeight, margin, name);
 
         this.plotMap();
+
+        this.latLng = [[49.19696140064054, -123.23123931884766], [49.31907993638747, -122.9871368408203]];
     }
 
     reset() {
-        this.plot.attr("d", this.path);
-
-        // this.grid.attr("transform", (d) => {
-        //     return "translate(" + d.key[0] + "," + d.key[1] + ")";
-        // });
-        //
-        // const bounds = this.path.bounds(this.geoData);
-        //
-        // const topLeft = bounds[0],
-        //     bottomRight = bounds[1];
-        //
-        // this.svg.attr("width", bottomRight[0] - topLeft[0])
-        //     .attr("height", bottomRight[1] - topLeft[1])
-        //     .style("left", topLeft[0] + "px")
-        //     .style("top", topLeft[1] + "px");
-        //
-        // // this.svg.attr("transform", "translate(" + -topLeft[0] + ","
-        // //     + -topLeft[1] + ")");
-        //
-        // // initialize the path data
-        // // d3_features.attr("d", path)
-        // //     .style("fill-opacity", 0.7)
-        // //     .attr('fill', 'blue');
+        // this.plot.attr("d", this.path);
     }
 
     plotMap() {
@@ -64,60 +45,86 @@ export class HeatMap extends AbstractPlot {
     }
 
     update(heatmapData: CrossFilter.Grouping<number[], number>[]) {
-        // const bounds = this.path.bounds(this.geoData);
-        //
-        // const topLeft = bounds[0],
-        //     bottomRight = bounds[1];
-        //
-        // this.width = bottomRight[0] - topLeft[0];
-        // this.height = bottomRight[1] - topLeft[1];
-        //
-        // console.log(heatmapData);
-
-        // let mappedData = heatmapData.map(d => {
-        //     const point = this.map.latLngToLayerPoint(new L.LatLng(d.key[1], d.key[0]));
-        //     d.key = [point.x, point.y];
-        //     return d;
-        // });
+        const widthTotal = Math.abs(this.latLng[0][0] - this.latLng[1][0]);
+        const heightTotal = Math.abs(this.latLng[0][1] - this.latLng[1][1])
+        const eachWidth = widthTotal / this.gridSize;
+        const eachHeight = heightTotal / this.gridSize;
 
         let filteredData = heatmapData.filter(d => {
             let i = d.key[0], j = d.key[1];
-            return i >= 0 && i < this.height / this.gridSize && j >= 0 && j < this.width / this.gridSize;
+            return i >= this.latLng[0][0] && i < this.latLng[1][0] && j >= this.latLng[0][1] && j < this.latLng[1][1];
         });
         let maxi = d3.max(filteredData, d => d.value);
 
         let colorScale = d3.scaleQuantize<string>().range(colors);
         colorScale.domain([0, maxi]);
 
-        let opacityScale = d3.scaleLinear().range([0.5, 0.85]);
+        let opacityScale = d3.scaleLinear().range([1]);
         opacityScale.domain([0, maxi]);
 
-        // Draw grid
-        this.grid = this.svg.selectAll("rect")
-            .data(filteredData);
+        const data = [];
 
-        this.grid.enter()
-            .append("rect")
-            .attr("width", this.gridSize)
-            .attr("height", this.gridSize)
-            .merge(this.grid)
-            .transition().duration(500)
-            .attr("x", d => d.key[0] * this.gridSize)
-            .attr("y", d => d.key[1] * this.gridSize)
-            .attr("fill", d => colorScale(d['value']))
-            .attr("opacity", d => d.value ? opacityScale(d.value) : 0)
-            .text(d => d['value']);
+        for (let row = 0; row < this.gridSize; row++) {
+            data[row] = [];
+            for (let column = 0; column < this.gridSize; column++) {
+                const bounds = [
+                    [this.latLng[0][0] + column * eachWidth, this.latLng[0][1] + row * eachHeight],
+                    [this.latLng[0][0] + (column + 1) * eachWidth, this.latLng[0][1] + (row + 1) * eachHeight]
+                ];
+                data[row][column] = {
+                    bounds: bounds,
+                    hits: 0
+                };
+            }
+        }
+        filteredData.forEach((d) => {
+            let found = false;
+            for (let i = 0; i < data.length && !found; i++) {
+                for (let j = 0; j < data[i].length && !found; j++) {
+                    if (
+                        d.key[0] >= data[i][j].bounds[0][0]
+                        && d.key[0] <= data[i][j].bounds[1][0]
+                        && d.key[1] >= data[i][j].bounds[0][1]
+                        && d.key[1] <= data[i][j].bounds[1][1]
+                    ) {
+                        data[i][j].hits += d.value;
+                        found = true;
+                    }
+                }
+            }
 
-        this.grid.exit().remove();
+            // let i = Math.round((d.key[0] - this.latLng[0][0]) / eachWidth);
+            // let j = Math.round((d.key[1] - this.latLng[0][1]) / eachHeight);
+            // if (i >= this.gridSize) {
+            //     i--;
+            // }
+            // if (j >= this.gridSize) {
+            //     j--;
+            // }
+            // data[i][j].hits += d.value;
+        });
+
+        // const test = L.rectangle([[49.248239, -123.118418], [49.268239, -123.098418]]);
+        // test.options.color = "black";
+        // test.addTo(this.map);
+
+        for (let i = 0; i < data.length; i++) {
+            for (let j = 0; j < data[i].length; j++) {
+                const rect = L.rectangle(data[i][j].bounds);
+                rect.options.color = colorScale(data[i][j].hits);
+                rect.options.weight = 0;
+                rect.options.fillOpacity = data[i][j].hits === 0 ? 0 : 1;
+                rect.options.className = "heatmap-rect";
+                rect.addTo(this.map);
+            }
+        }
+
+        // console.log(data);
     }
 
     createGridDimension(crimes: CrossFilter.CrossFilter<Crime>) {
         let crimesByGridDimension = crimes.dimension(d => {
-            const point = this.map.latLngToLayerPoint(new L.LatLng(d.Y, d.X));
-            // d.key = [Math.round(point.x / this.gridSize), Math.round(point.y / this.gridSize)];
-
-            return [Math.round(point.x / this.gridSize), Math.round(point.y / this.gridSize)];
-            // return [d.X, d.Y];
+            return [d.Y, d.X];
         });
 
         return crimesByGridDimension;
